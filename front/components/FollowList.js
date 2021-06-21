211588d1b7c136fa15b99a54430f1127d25fa674
sungyun <sungyun5423@gmail.com>
2021-06-22 07:58:07 +0900

    [Style] create profile page

---
+++ front/components/FollowList.js
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import { List, Card, Button } from "antd";
+import { StopOutlined } from "@ant-design/icons";
+
+const FollowList = ({ header, data }) => (
+  <List
+    style={{ marginBottom: "20px" }}
+    grid={{ gutter: 4, xs: 2, md: 3 }}
+    size="small"
+    header={<div>{header}</div>}
+    loadMore={
+      <div style={{ textAlign: "center", margin: "10px 0" }}>
+        <Button>더 보기</Button>
+      </div>
+    }
+    bordered
+    dataSource={data}
+    renderItem={(item) => (
+      <List.Item style={{ marginTop: "20px" }}>
+        <Card actions={[<StopOutlined key="stop" />]}>
+          <Card.Meta description={item.nickname} />
+        </Card>
+      </List.Item>
+    )}
+  />
+);
+
+export default FollowList;
+
+FollowList.propTypes = {
+  header: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
+};
