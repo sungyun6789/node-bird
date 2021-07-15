7c206b4b3ff50c0add6e64ed258d2b9f5da26b8f
sungyun <sungyun5423@gmail.com>
2021-07-15 20:52:19 +0900

    [ReduxSaga] faker를 사용해 더미데이터 만들기

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -3,9 +3,20 @@ import AppLayout from '../components/AppLayout';
 import NicknameEditForm from '../components/NicknameEditForm';
 import FollowList from '../components/FollowList';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import Router from 'next/router';
 
 const Profile = () => {
   const { me } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (!(me && me.id)) {
+      Router.push('/');
+    }
+  }, [me && me.id]);
+  if (!me) {
+    return null;
+  }
 
   return (
     <>
