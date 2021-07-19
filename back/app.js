992c5fa9dc2a3584ecb3453ad6ec26621bdca89b
sungyun <sungyun5423@gmail.com>
2021-07-20 00:14:13 +0900

    [Node] express router separated

--- back/app.js
+++ back/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const postRouter = require("./routes/post");
 
 const app = express();
 
@@ -16,11 +17,11 @@ app.get("/", (req, res) => {
   res.send("hello express");
 });
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.send("hello api");
 });
 
-app.get("/api/posts", (req, res) => {
+app.get("/posts", (req, res) => {
   res.json([
     { id: 1, content: "hello1" },
     { id: 2, content: "hello2" },
@@ -28,13 +29,7 @@ app.get("/api/posts", (req, res) => {
   ]);
 });
 
-app.post("/api/post", (req, res) => {
-  res.json({ id: 1, content: "success" });
-});
-
-app.delete("/api/post", (req, res) => {
-  res.send("delete");
-});
+app.use("/post", postRouter);
 
 app.listen(3065, () => {
   console.log("서버 실행 중");
