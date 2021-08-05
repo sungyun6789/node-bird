156da9f26f9827eecac8110be2cf654f8e677a44
sungyun <sungyun5423@gmail.com>
2021-08-05 21:39:03 +0900

    [Next] SSR cookie share

--- front/pages/index.js
+++ front/pages/index.js
@@ -8,6 +8,7 @@ import { LOAD_POSTS_REQUEST } from '../reducers/post';
 import { LOAD_USER_REQUEST } from '../reducers/user';
 
 import wrapper from '../store/configureStore';
+import axios from 'axios';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -50,6 +51,11 @@ const Home = () => {
 
 // front => back, browser가 중간에 없이 통신
 export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+  const cookie = context.req ? context.req.headers.cookie : '';
+  axios.defaults.headers.Cookie = '';
+  if (context.req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
   context.store.dispatch({
     type: LOAD_USER_REQUEST,
   });
